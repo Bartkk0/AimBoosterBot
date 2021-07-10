eae6cec96afdbc45b511c158f4dc160c1d64263e
Bartkk0 <32180149+Bartkk0@users.noreply.github.com>
2021-07-10 05:35:54 +0200

    Update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import keyboard
 import mouse
 import numpy as np
@@ -5,11 +7,22 @@ from PIL import ImageGrab
 from cv2 import cv2
 
 X = 182
-Y = 383
+Y = 369
 W = 596
 H = 416
 
+SLEEP_AFTER_CLICK = 0.1
+SLEEP_AFTER_ITER = 0.3
+
 while True:
+    # Check if q is pressed to exit
+    if keyboard.is_pressed('q'):
+        break
+
+    # Check if e is pressed to pause
+    if keyboard.is_pressed('e'):
+        continue
+
     # Grab a screenshot of the game
     screen = ImageGrab.grab((X, Y, X + W, Y + H)).convert('RGB')
     # Convert it to a numpy array
@@ -20,20 +33,15 @@ while True:
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # Set the color range
-    lower = np.array([0, 0, 0])
-    upper = np.array([128, 128, 128])
+    lower = np.array([23 / 2, (23 / 100) * 255, (100 / 100) * 255])
+    upper = np.array([25/2, (70/100)*255, (100/100)*255])
 
     # Detect circles and invert the result
     mask = cv2.inRange(hsv, lower, upper)
-    mask = cv2.bitwise_not(mask)
 
     # Detect contours in the result
     contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[0] if len(contours) == 2 else contours[1]
-
-    # Check if q is pressed to exit
-    if keyboard.is_pressed('q'):
-        break
 
     for c in contours:
         # Check the contour area
@@ -52,3 +60,7 @@ while True:
         # Move the mouse and click
         mouse.move(X + centerX, Y + centerY)
         mouse.click('left')
+
+        time.sleep(SLEEP_AFTER_CLICK)
+
+    time.sleep(SLEEP_AFTER_ITER)
